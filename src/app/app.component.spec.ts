75ed818bee57335ee04d49dc9d4757cd3a2e1516
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-04-24 22:48:05 -0500

    AZUL: Se añaden componentes en app.component.spec.ts para eliminar errores

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AlbumModule } from './album/album.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AlbumModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        SidebarComponent,
+        FooterComponent
       ],
     }).compileComponents();
   }));
