5fdd36c24c78e7072855729545eb1b68610a77f3
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-04-24 23:51:30 -0500

    AZUL: Verifica que la subscripcion retorne un arreglo de albumes mock

--- src/app/album/album.service.spec.ts
+++ src/app/album/album.service.spec.ts
@@ -5,6 +5,8 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { environment } from '../../environments/environment';
+import faker from 'faker'
+import { Album, GENRE } from './album';
 
 describe('AlbumService', () => {
   let service: AlbumService;
@@ -51,6 +53,27 @@ describe('AlbumService', () => {
     serviceSpy.get.and.returnValue(of([]))
     const sub = service.obtenerAlbums().subscribe();
     expect(serviceSpy.get.calls.allArgs()[0][0]).toBe(environment.backUrl+'Albums')
+    sub.unsubscribe();
+  })
+
+
+  it('la subscripcion retorna el arreglo mock de albumes',()=>
+  {
+    service = null;
+    service = new AlbumService(serviceSpy as any);
+    let arrayMock = []
+    for (let i = 1; i < 10; i++) {
+      let albumMock = new Album(faker.name.firstName(),faker.image.imageUrl(),faker.date.past(),faker.lorem.text(),faker.random.number({'min':0,'max':3}),faker.random.number({'min':0,'max':4}))
+      arrayMock.push(albumMock);
+    }
+   
+
+    serviceSpy.get.and.returnValue(of(arrayMock))
+    const sub = service.obtenerAlbums().subscribe(al =>
+      {
+        expect(al).toBe(arrayMock);
+      }
+      );
     sub.unsubscribe();
   })
 
