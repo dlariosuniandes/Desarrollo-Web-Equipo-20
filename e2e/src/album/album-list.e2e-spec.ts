0d002226dba2dbb4cfd4a08135afad87f8bdac22
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-05-23 00:37:47 -0500

    Se añade pruebas e2e de HU002

--- e2e/src/album/album-list.e2e-spec.ts
+++ e2e/src/album/album-list.e2e-spec.ts
@@ -8,29 +8,101 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('El boton de agregar album dispara el componente de crear album en un modal', async() => {
-    page.navigateTo();
-    browser.waitForAngular()
-    page.darClickAgregarAlbum()
-    browser.waitForAngular()
-    expect(page.verificaExistenciaDeModal('albumCreationModal')).toBeTruthy(); 
-  });
-
-  it('El boton ver detalle Album navega hasta el detalle de un album y regresa al listado', async() => {
-    page.navigateTo();
-    browser.manage().window().maximize();
-    browser.waitForAngular();
-    let numberCards:number
-    numberCards = await page.obtenerTamanoCartasAlbums()
-    for(let i=0; i<numberCards;i++)
+  for(let i=0;i<9;i++)
+  {
+    it('El boton de agregar album dispara el componente de crear album en un modal', async() => {
+      await page.navigateTo();
+      await browser.waitForAngular()
+      await page.darClickAgregarAlbum()
+      await browser.waitForAngular()
+      expect(await page.verificaExistenciaDeModal('albumCreationModal')).toBeTruthy(); 
+    });
+  
+    it('Crea un album y espera que se visualice en el listado', async() => {
+      await page.navigateTo();
+      await browser.waitForAngular()
+      await page.darClickAgregarAlbum()
+      await browser.waitForAngular()
+      let numberCardsInit = await page.obtenerTamanoCartasAlbums()
+      await page.llenarFormularioAlbum();
+      await page.clickSubmitButton();
+      let numberCardsEnd = await page.obtenerTamanoCartasAlbums()
+      expect(await numberCardsInit < await numberCardsEnd).toBeTruthy()
+  
+    });
+  
+    it('El boton ver detalle Album navega hasta el detalle de un album y regresa al listado', async() => {
+      await page.navigateTo();
+      await browser.manage().window().maximize();
+      await browser.waitForAngular();
+      let numberCards:number
+      numberCards = await page.obtenerTamanoCartasAlbums()
+      for(let i=0; i<numberCards;i++)
+      {
+        const idAlbum = await page.detallarAlbum(i);
+        await browser.waitForAngular();
+        await page.verificarUrl(await idAlbum);
+        await browser.waitForAngular();
+        await page.volverListaAlbums();
+        await browser.waitForAngular();
+      }
+  
+    });
+  
+    it('Crea un track en el primer album', async ()=>
     {
-      const idAlbum = await page.detallarAlbum(i);
-      browser.waitForAngular();
-      page.verificarUrl(idAlbum);
-      browser.waitForAngular();
-      page.volverListaAlbums();
-    }
-  });
+      await page.navigateTo();
+      await browser.manage().window().maximize();
+      await browser.waitForAngular();
+      const idAlbum = await page.detallarAlbum(0);
+      await browser.waitForAngular();
+      const numberTracksInit = await page.contarTracks();
+      await page.verificarUrl(await idAlbum);
+      await page.clickAgregarTrack();
+      await browser.waitForAngular();
+      await page.llenarFormularioTrack();
+      await browser.waitForAngular();
+      const numberTracksEnd = await page.contarTracks();
+      expect(await numberTracksInit<numberTracksEnd).toBeTruthy();
+    })
+  
+    it('Elimina el ultimo track del primer album',async ()=>
+    {
+      await page.navigateTo();
+      await browser.manage().window().maximize();
+      await browser.waitForAngular();
+      const idAlbum = await page.detallarAlbum(0);
+      await browser.waitForAngular();
+      const numberTracksInit = await page.contarTracks();
+      await page.verificarUrl(await idAlbum);
+      await page.eliminarTrack();
+      await browser.waitForAngular();
+      const numberTracksEnd = await page.contarTracks();
+      expect(await numberTracksInit>numberTracksEnd).toBeTruthy();
+
+    })
+ 
+  
+    it('Elimina el ultimo album de la lista', async ()=>
+    {
+      await page.navigateTo();
+      await browser.manage().window().maximize();
+      await browser.waitForAngular();
+      let numberCardsInit =  await page.obtenerTamanoCartasAlbums()
+      const idAlbum = await page.detallarAlbum((await numberCardsInit)-1);
+      await browser.waitForAngular();
+      await page.verificarUrl(await idAlbum);
+      await page.eliminarAlbum();
+      await browser.waitForAngular();
+      let numberCardsEnd =  await page.obtenerTamanoCartasAlbums();
+      expect(await numberCardsEnd< await numberCardsInit).toBeTruthy();
+      
+    })
+  }
+
+
+
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,5 +1,6 @@
 import { browser, by, element, ElementFinder } from 'protractor';
 import { protractor } from 'protractor/built/ptor';
+import {faker} from 'faker';
 
 export class AppPage {
   navigateTo(childComponents?:string): Promise<unknown> {
@@ -43,9 +44,10 @@ export class AppPage {
     let idAlbum:string
     await this.esperarSelector(element(by.xpath(`//button[@id="buttonAlbum${index}"]`)))
     let elHijo = await element(by.xpath(`//button[@id="buttonAlbum${index}"]`))
-    browser.actions().mouseDown(elHijo)
+    browser.actions().mouseMove(await elHijo)
     idAlbum= await elHijo.element(by.xpath('../../..')).getAttribute('id')
-    let splitted = idAlbum.split("-")
+    let splitted = await idAlbum.split("-")
+    this.esperar(200)
     await elHijo.click()
     return Number(splitted[1])
   }
@@ -57,12 +59,102 @@ export class AppPage {
 
   async verificarUrl(index:number)
   {
-    const actualUrl = browser.getCurrentUrl();
-    expect(actualUrl).toEqual(`http://localhost:4200/albums/${index}`);
+    const actualUrl = await browser.getCurrentUrl();
+    expect(await actualUrl).toEqual(`${browser.baseUrl}albums/${index}`);
   }
 
   async volverListaAlbums()
   {
-    element(by.id('iconBack')).click()
+    await element(by.id('iconBack')).click()
+  }
+
+   async llenarFormularioAlbum()
+  {
+    await browser.wait(element.all(by.css('input')).isPresent(),5000);
+    const inputArray = await element.all(by.xpath("//input"))
+    this.esperar(200)
+    await inputArray[0].click()
+    await inputArray[0].sendKeys('Musica para volar');
+    await inputArray[1].click()
+    await inputArray[1].sendKeys('https://images-na.ssl-images-amazon.com/images/I/71OmC6lSOcL._SL1500_.jpg');
+    this.esperar(200)
+    await element(by.xpath("//input[@type='date']")).sendKeys('11/05/2021');
+    this.esperar(200)
+    await element(by.css('textarea')).click()
+    await element(by.css('textarea')).sendKeys('adgfkjhfjhadfnkebhnarfusvnauidgfbaiubgfdsvnfbahuibgfdsabgnfhuib')
+    this.esperar(200)
+    const selector = await element(by.xpath("//select[@aria-label='Default select example']"))
+    await selector.click()
+    const option = await selector.element(by.xpath('//option[@value="2: Folk"]'))
+    await option.click()
+    const selector2 = await element(by.xpath("//select[@aria-label='seleccion de sello']"))
+    await selector2.click()
+    const option2 = await selector.element(by.xpath('//option[@value="2: Elektra"]'))
+    await option2.click()
+  }
+
+  async clickSubmitButton()
+  {
+    await browser.wait(element(by.xpath('//button[@type="submit"]')).isEnabled(),5000)
+    await element(by.xpath('//button[@type="submit"]')).click()
+    await browser.wait(element(by.css('.swal2-confirm.swal2-styled')).isPresent(),5000)
+    await element(by.css('button.swal2-confirm.swal2-styled')).click()
+  }
+
+  async eliminarAlbum()
+  {
+    await browser.wait(element(by.css('.bi.bi-trash')).isDisplayed(),5000)
+    await element(by.css('.bi.bi-trash')).click()
+    await browser.wait(element(by.css('.swal2-confirm.swal2-styled.swal2-default-outline')).isPresent(),5000);
+    await element(by.css('.swal2-confirm.swal2-styled.swal2-default-outline')).click();
+    await browser.wait(element(by.css('.swal2-confirm.swal2-styled')).isDisplayed(),5000);
+    await element(by.css('button.swal2-confirm.swal2-styled')).click();
+    await browser.waitForAngular();
+  }
+
+  async clickAgregarTrack()
+  {
+    await browser.wait(element(by.xpath('//button[@data-target="#trackCreationModal"]')).isDisplayed(),5000)
+    await element(by.xpath('//button[@data-target="#trackCreationModal"]')).click();
+  }
+  async llenarFormularioTrack(){
+    await browser.wait(element(by.id('trackName')).isPresent(),5000)
+    this.esperar(200)
+    await element(by.id('trackName')).sendKeys('Nuevo Track')
+    this.esperar(200)
+    await element(by.id('trackDuration')).sendKeys('250')
+    await browser.wait(element(by.xpath('//button[@type="submit"]')).isEnabled(),5000)
+    await element(by.xpath('//button[@type="submit"]')).click()
+    await browser.wait(element(by.css('.swal2-confirm.swal2-styled')).isDisplayed(),5000);
+    await element(by.css('button.swal2-confirm.swal2-styled')).click();
+  }
+
+  async contarTracks()
+  {
+    let rtaBool=await element(by.xpath('//*[@id="tackTable"]')).isPresent()
+    if(await rtaBool)
+    {
+      const rowTracks = await element.all(by.xpath('//*[@id="tackTable"]/table/tbody/tr')).count()
+      return await rowTracks
+    }
+    else
+    {
+      return await 0;
+    }
+  }
+
+  async eliminarTrack()
+  {
+    let rtaBool=await element(by.xpath('//*[@id="tackTable"]')).isPresent()
+    const rowTracks = await element.all(by.xpath('//*[@id="tackTable"]/table/tbody/tr')).count()
+    this.esperar(200)
+    const tRow = await element.all(by.xpath('//*[@id="tackTable"]/table/tbody/tr'))
+    await tRow[(await rowTracks)-1].element(by.xpath('//td/button')).click();
+    await browser.wait(element(by.css('.swal2-confirm.swal2-styled.swal2-default-outline')).isPresent(),5000)
+    this.esperar(200)
+    await element(by.css('.swal2-confirm.swal2-styled.swal2-default-outline')).click()
+    await browser.wait(element(by.css('.swal2-confirm.swal2-styled.swal2-default-outline')).isPresent(),5000)
+    this.esperar(200)
+    await element(by.css('.swal2-confirm.swal2-styled.swal2-default-outline')).click()
   }
 }
