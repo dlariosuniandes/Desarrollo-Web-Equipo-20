508a8090e170ffdb12b285e1b5adbc9c5cf16fdd
Daniel Larios <d.larios@uniandes.edu.co>
2021-05-28 00:21:12 +0200

    se añaden mas pruebas unitarias

--- src/app/album/album-detail/album-detail.component.spec.ts
+++ src/app/album/album-detail/album-detail.component.spec.ts
@@ -1,141 +1,272 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlbumDetailComponent } from './album-detail.component';
-import { HttpClientTestingModule} from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppRoutingModule } from '../../app-routing.module';
 import { DebugElement } from '@angular/core';
 import { Album, Track } from '../album';
-import faker from "faker";
+import faker from 'faker';
 import { Comment } from 'src/app/comentario/comment';
 import { Band } from '../../perfomer/band';
 import { Musician } from '../../perfomer/musician';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { Router } from '@angular/router';
 
 describe('AlbumDetailComponent', () => {
   let component: AlbumDetailComponent;
-  let fixture: ComponentFixture<AlbumDetailComponent>
-  let debElement: DebugElement
-  let htmlElement: HTMLElement
-  let albumMock: Album
+  let fixture: ComponentFixture<AlbumDetailComponent>;
+  let debElement: DebugElement;
+  let htmlElement: HTMLElement;
+  let albumMock: Album;
+  let router: jasmine.SpyObj<Router>;
 
-  let crearMockAlbum = ()=>
-  {
-    let arrayTrackMock = []
-    let arrayArtist=[]
-    let arrayComments=[]
-    for(let n = 1; n<10;n++)
-    {
-
-      arrayTrackMock.push(new Track(faker.datatype.number({'min': 100, 'max': 300}),faker.name.firstName(),faker.lorem.text()));
-      if(n%2 == 0)
-      {
-        
-        arrayArtist.push(new Band(faker.date.past(),faker.name.firstName(),faker.lorem.text(),faker.datatype.number({'min': 100, 'max': 300}),faker.image.imageUrl()))
+  let crearMockAlbum = (condition: boolean) => {
+    let arrayTrackMock = [];
+    let arrayArtist = [];
+    let arrayComments = [];
+    for (let n = 1; n < 10; n++) {
+      arrayTrackMock.push(
+        new Track(
+          faker.datatype.number({ min: 100, max: 300 }),
+          faker.name.firstName(),
+          faker.lorem.text()
+        )
+      );
+      if (n % 2 == 0) {
+        arrayArtist.push(
+          new Band(
+            faker.date.past(),
+            faker.name.firstName(),
+            faker.lorem.text(),
+            faker.datatype.number({ min: 100, max: 300 }),
+            faker.image.imageUrl()
+          )
+        );
+        if (condition) {
+          arrayArtist.push(
+            new Band(
+              faker.date.past(),
+              faker.name.firstName(),
+              faker.lorem.text(),
+              faker.datatype.number({ min: 100, max: 300 }),
+              faker.image.imageUrl()
+            )
+          );
+        }
+      } else {
+        arrayArtist.push(
+          new Musician(
+            faker.date.past(),
+            faker.name.firstName(),
+            faker.lorem.text(),
+            faker.datatype.number({ min: 100, max: 300 }),
+            faker.image.imageUrl()
+          )
+        );
+        if (condition) {
+          arrayArtist.push(
+            new Band(
+              faker.date.past(),
+              faker.name.firstName(),
+              faker.lorem.text(),
+              faker.datatype.number({ min: 100, max: 300 }),
+              faker.image.imageUrl()
+            )
+          );
+        }
       }
-      else
-      {
-        arrayArtist.push(new Musician(faker.date.past(),faker.name.firstName(),faker.lorem.text(),faker.datatype.number({'min': 100, 'max': 300}),faker.image.imageUrl()))
-      }
-      arrayComments.push(new Comment(faker.datatype.number({'min': 100, 'max': 300}),faker.lorem.text(),faker.datatype.number({'min': 0, 'max': 5})));
+      arrayComments.push(
+        new Comment(
+          faker.datatype.number({ min: 100, max: 300 }),
+          faker.lorem.text(),
+          faker.datatype.number({ min: 0, max: 5 })
+        )
+      );
     }
 
-    albumMock = new Album(faker.datatype.number({'min': 100, 'max': 300}),faker.name.firstName(),faker.image.imageUrl(),faker.date.past(),faker.lorem.text(),faker.datatype.number({'min':0,'max':3}),faker.datatype.number({'min':0,'max':4}),arrayTrackMock,arrayArtist,arrayComments)
+    albumMock = new Album(
+      faker.datatype.number({ min: 100, max: 300 }),
+      faker.name.firstName(),
+      faker.image.imageUrl(),
+      faker.date.past(),
+      faker.lorem.text(),
+      faker.datatype.number({ min: 0, max: 3 }),
+      faker.datatype.number({ min: 0, max: 4 }),
+      arrayTrackMock,
+      arrayArtist,
+      arrayComments
+    );
+  };
 
-  }
-  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlbumDetailComponent ],
-      imports: [HttpClientTestingModule, RouterTestingModule]
-    })
-    .compileComponents();
+      declarations: [AlbumDetailComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AlbumDetailComponent);
     component = fixture.componentInstance;
-    component.idAlbum = 100
+    component.idAlbum = 100;
     debElement = fixture.debugElement;
     htmlElement = debElement.nativeElement;
     fixture.detectChanges();
-
+    router = TestBed.get(Router);
+    spyOn(router, 'navigateByUrl');
+    crearMockAlbum(false);
+    component.albumDetail = albumMock;
+    spyOn(component.albumDetail, 'darId').and.returnValue(100)
+    spyOn(router, 'navigate');
   });
 
-  afterEach(()=>
-  {
-    albumMock = undefined
-    component = null
-    fixture = null
-  })
+  afterEach(() => {
+    albumMock = undefined;
+    component = null;
+    fixture = null;
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Verifica que las funciones subServiceAlbum y obtenerIdRuta sean llamadas al inicializar el componente',()=>
-  {
-    const spy = {func1: spyOn(component,'subServiceAlbum'), func2: spyOn(component,'obtenerIdRuta')}
+  it('Verifica que las funciones subServiceAlbum y obtenerIdRuta sean llamadas al inicializar el componente', () => {
+    const spy = {
+      func1: spyOn(component, 'subServiceAlbum'),
+      func2: spyOn(component, 'obtenerIdRuta'),
+    };
     component.ngOnInit();
     expect(spy.func1.calls.count()).toBe(1);
     expect(spy.func2.calls.count()).toBe(1);
   });
 
-  it('Verifica que al inicializar el componente la variable albumI sea instancia de album',()=>
-  {
-    crearMockAlbum();
-    const spy = spyOn(component,'subServiceAlbum');
-    let funcTest = ()=>
-    {
-      component.albumDetail=albumMock;
-    }
+  it('Verifica que al inicializar el componente la variable albumI sea instancia de album', () => {
+    crearMockAlbum(false);
+    const spy = spyOn(component, 'subServiceAlbum');
+    let funcTest = () => {
+      component.albumDetail = albumMock;
+    };
     component.ngOnInit();
-    if(spy.calls.count()>0)
-    {
+    if (spy.calls.count() > 0) {
       funcTest();
     }
     expect(component.albumDetail instanceof Album).toBeTrue();
-    
   });
 
-  it('Verifica que se pinte el detalle del album en el html',()=>
-  {
-    crearMockAlbum();
-    const spy = spyOn(component,'subServiceAlbum');
-    let funcTest = ()=>
-    {
-      component.albumDetail=albumMock;
-    }
+  it('Verifica que se pinte el detalle del album en el html', () => {
+    crearMockAlbum(false);
+    const spy = spyOn(component, 'subServiceAlbum');
+    let funcTest = () => {
+      component.albumDetail = albumMock;
+    };
     component.ngOnInit();
-    if(spy.calls.count()>0)
-    {
+    if (spy.calls.count() > 0) {
       funcTest();
     }
     fixture.detectChanges();
-    const urlImg: string = htmlElement.querySelector('#albumImg').parentNode.querySelector('img').src;
-    const numberTracks = htmlElement.querySelector('tbody.mb-0').querySelectorAll('tr').length
-    const numberComments = htmlElement.querySelectorAll("#commentCollector").length
+    const urlImg: string = htmlElement
+      .querySelector('#albumImg')
+      .parentNode.querySelector('img').src;
+    const numberTracks = htmlElement
+      .querySelector('tbody.mb-0')
+      .querySelectorAll('tr').length;
+    const numberComments =
+      htmlElement.querySelectorAll('#commentCollector').length;
     expect(urlImg).toEqual(component.albumDetail.darPortada());
     expect(numberTracks).toEqual(component.albumDetail.darTracks().length);
-    expect(numberComments).toEqual(component.albumDetail.darComentarios().length)
+    expect(numberComments).toEqual(
+      component.albumDetail.darComentarios().length
+    );
   });
 
-  it('Verifica que al dar click en el boton de back, se active la funcion backList',()=>
-  {
-    crearMockAlbum();
-    const spy1 = spyOn(component,'subServiceAlbum');
-    let funcTest = ()=>
-    {
-      component.albumDetail=albumMock;
-    }
+  it('Verifica que al dar click en el boton de back, se active la funcion backList', () => {
+    crearMockAlbum(false);
+    const spy1 = spyOn(component, 'subServiceAlbum');
+    let funcTest = () => {
+      component.albumDetail = albumMock;
+    };
     component.ngOnInit();
-    if(spy1.calls.count()>0)
-    {
+    if (spy1.calls.count() > 0) {
       funcTest();
     }
     fixture.detectChanges();
-    let spy2 = spyOn(component,'backList');
-    htmlElement.querySelector("#iconBack").parentNode.querySelector("i").click();
+    let spy2 = spyOn(component, 'backList');
+    htmlElement
+      .querySelector('#iconBack')
+      .parentNode.querySelector('i')
+      .click();
     expect(spy2.calls.count()).toBe(1);
+  });
+
+  it('expect backList to navigate back if it has a backUrl', () => {
+    component.backUrl = 'test';
+    component.backList();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('test');
+  });
+
+  it('expect backList to navigate back if it has a backUrl', () => {
+    component.backUrl = undefined;
+    component.backList();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('albums/list');
+  });
+
+  it('expect to get artist string if theres only 1 artist on the array', () => {
+    crearMockAlbum(false);
+    component.albumDetail = albumMock;
+    component.stringArtistas();
+    spyOn(component.albumDetail, 'darArtistas');
+    expect(component.albumDetail.darArtistas).toBeTruthy();
+  });
+  it('expect to get artist string if theres more than 1 artist on the array', () => {
+    crearMockAlbum(true);
+    component.albumDetail = albumMock;
+    component.stringArtistas();
+    spyOn(component.albumDetail, 'darArtistas');
+    expect(component.albumDetail.darArtistas).toBeTruthy();
+  });
+
+  it('expect to get artist string if theres more than 1 artist on the array', () => {
+    crearMockAlbum(true);
+    component.albumDetail = albumMock;
+    component.stringArtistas();
+    spyOn(component.albumDetail, 'darArtistas');
+    expect(component.albumDetail.darArtistas).toBeTruthy();
+  });
+
+  it('should navigate to band if band instance is given', () => {
+    component.navegarArtista(
+      new Musician(
+        faker.date.past(),
+        faker.name.firstName(),
+        faker.lorem.text(),
+        faker.datatype.number({ min: 100, max: 300 }),
+        faker.image.imageUrl()
+      )
+    );
+    expect(router.navigateByUrl).toHaveBeenCalled()
+  });
+
+  it('should navigate to musician if musician instance is given', () => {
+    component.navegarArtista(
+      new Band(
+        faker.date.past(),
+        faker.name.firstName(),
+        faker.lorem.text(),
+        faker.datatype.number({ min: 100, max: 300 }),
+        faker.image.imageUrl()
+      )
+    );
+    expect(router.navigateByUrl).toHaveBeenCalled()
+  });
+
+  it('change creating tag should toggle flag', () => {
+    component.creatingTrackOn = false
+    component.changeCreatingTrack()
+    expect(component.creatingTrackOn).toBeTruthy()
+  })
+
+  it('reload component shoould navigate using current url', () => {
+    component.reloadComponent()
+    expect(router.navigate).toHaveBeenCalled()
   })
 });
--- src/app/perfomer/list/list.component.spec.ts
+++ src/app/perfomer/list/list.component.spec.ts
@@ -7,11 +7,13 @@ import { Musician } from '../musician';
 import { By } from '@angular/platform-browser';
 import { Band } from '../band';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
   let debug: DebugElement;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -43,10 +45,16 @@ describe('ListComponent', () => {
         'https://url.com',
         [],
         []
-      )
-    ]
+      ),
+    ];
     fixture.detectChanges();
     debug = fixture.debugElement;
+    router = TestBed.get(Router);
+    spyOn(router, 'getCurrentNavigation').and.returnValue({
+      extras: { state: { backUrl: 'msg' } },
+    } as any);
+    spyOn(router, 'navigateByUrl')
+    spyOn(router, 'navigate');
   });
 
   it('should create', () => {
@@ -59,19 +67,64 @@ describe('ListComponent', () => {
     );
   });
 
-  it('should have a title musician',()=> {
-    expect(debug.query(By.css(".list-title.musician")).nativeElement.innerText).toBe("Musicos")
-  })
+  it('should have a title musician', () => {
+    expect(
+      debug.query(By.css('.list-title.musician')).nativeElement.innerText
+    ).toBe('Musicos');
+  });
 
-  it('should have a musician card',()=> {
-    expect(debug.query(By.css('.musician-card')).nativeElement).toBeTruthy()
-  })
+  it('should have a musician card', () => {
+    expect(debug.query(By.css('.musician-card')).nativeElement).toBeTruthy();
+  });
 
-  it('should have a title tag for bands',()=> {
-    expect(debug.query(By.css(".list-title.band")).nativeElement.innerText).toBe("Bandas")
-  })
+  it('should have a title tag for bands', () => {
+    expect(
+      debug.query(By.css('.list-title.band')).nativeElement.innerText
+    ).toBe('Bandas');
+  });
 
-  it('should have a band card',()=> {
-    expect(debug.query(By.css('.band-card')).nativeElement).toBeTruthy()
+  it('should have a band card', () => {
+    expect(debug.query(By.css('.band-card')).nativeElement).toBeTruthy();
+  });
+
+  it('expect onSelect to navigate to musician detail', () => {
+    component.onSelect(
+      new Musician(
+        new Date(),
+        'Test',
+        'Musico Test',
+        100,
+        'https://url.com',
+        [],
+        []
+      )
+    );
+    expect(router.navigateByUrl).toHaveBeenCalledWith('performers/musician/' + 100)
+  });
+
+  it('expect onSelect to navigate to band detail', () => {
+    component.onSelect(
+      new Band(
+        new Date(),
+        'Test',
+        'Musico Test',
+        100,
+        'https://url.com',
+        [],
+        []
+      )
+    );
+    expect(router.navigateByUrl).toHaveBeenCalledWith('performers/band/' + 100)
+  });
+
+  it('expect toggle function to change flag', () => {
+    const initialValue = component.flagModal;
+    component.modalToggle();
+    expect(component.flagModal).toBe(!initialValue);
+  });
+
+  it('should test reload component function', () =>{
+    component.reloadComponent()
+    expect(router.navigate).toHaveBeenCalled()
   })
 });
